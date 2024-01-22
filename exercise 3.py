8c521728a3524b06f0a25ab8e081d033f672ac05
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-22 10:44:42 +0800

    printing another given sentence

--- exercise 3.py
+++ exercise 3.py
@@ -4,3 +4,5 @@ word = input("What is the word you want to slice?: ")
 
 # print the given in the sample question
 print("Original string is", word)
+#also this one
+print("Printing only even index cards")
